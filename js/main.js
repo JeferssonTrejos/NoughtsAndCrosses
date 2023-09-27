9ed7069d31b185b5fc0920699c1d21d6b6976530
Jefersson <92814700+JeferssonTrejos@users.noreply.github.com>
2023-09-27 11:06:09 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
+//BOTONES
+const botonMostrar = document.getElementById("mostrarMenu");
+const botonCerrar = document.getElementById("cerrar");
 
+//MENU EMERGENTE
+const menu = document.getElementById("menu");
+
+botonMostrar.addEventListener("click", function () {
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
+botonCerrar.addEventListener("click", function () {
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
+
+
+
+//Cambia de color y tamaño los cuadros de los nombres(Es de prueba)
+const playerTurn = document.getElementById("siguiente");
+const player_1 = document.getElementById("player-1");
+const player_2 = document.getElementById("player-2");
+
+let lightPurple = '#534f9c';
+let darkPurple = '#534f9ca5';
+
+playerTurn.addEventListener('click',playerTurns)
+
+function playerTurns(){
+    addtexttest()
+    if (player_1.style.scale == 1.2) {
+        player_1.style.scale = 1
+        player_2.style.scale = 1.2
+        
+        player_1.style.backgroundColor = darkPurple;
+        player_2.style.backgroundColor = lightPurple;
+    }else{
+        player_2.style.scale = 1
+        player_1.style.scale = 1.2   
+        
+        player_1.style.backgroundColor = lightPurple;
+        player_2.style.backgroundColor = darkPurple;
+    }
+};
+
+//Agrega texto al log(Es de prueba)
+
+const log = document.getElementById('game-log');
+function addtexttest(){
+    log.insertAdjacentHTML('beforeend',`
+    <h6>- Turno de KKKK</h6>
+    `)
+}
